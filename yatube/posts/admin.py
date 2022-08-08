08c9d6e43025c063436f9e69bb54a7b065d2fdc1
Sergey Trunov <sergeytru15@gmail.com>
2022-08-08 16:05:07 +1000

    Добавлена модель Post.

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk','text', 'pub_date', 'author')
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Post, PostAdmin)
