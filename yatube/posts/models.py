d0dbe9ccb8c83473afbb15143d9602f85c0f5c08
Sergey Trunov <sergeytru15@gmail.com>
2022-08-08 21:30:59 +0900

    Добавлена модель Group.

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,7 +5,17 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.TextField()
+    slug = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    group = models.ForeignKey(Group, blank=True, null=True, on_delete=models.CASCADE)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,6 @@
 # posts/views.py
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 def index(request):
@@ -12,8 +12,10 @@ def index(request):
 
 
 def group_posts(request, slug):
-    template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'title': "Здесь будет информация о группах проекта Yatube"
+        'group': group,
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context)
