810c999f7b73cd93399ba222187b464b4a7fafad
Trunov Sergey <sergeytru15@gmail.com>
2022-09-17 13:20:43 +0900

    Add pages of errors.

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -1,5 +1,8 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +11,12 @@ urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
     path('about/', include('about.urls', namespace='about')),
 ]
+
+handler403 = 'core.views.permission_denied'
+handler404 = 'core.views.page_not_found'
+handler500 = 'core.views.server_error'
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
